2507b7e77b74e3b96b01e35b750f40b2c51fe2ef
Aldwin <142070350+aldddddddd@users.noreply.github.com>
2024-01-18 18:45:27 +0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Jovell Christian Q. Miranda';
+const name = 'Aldwin S. Española';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
